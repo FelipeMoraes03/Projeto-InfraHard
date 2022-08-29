79396e6de6bcf2dc1269bfabe9d4ce3c72a8f2d5
Pedro Fonseca <86477558+pedroalfonseca@users.noreply.github.com>
2022-08-29 10:43:50 -0300

    Update shift_left_2_PC.v

--- modulos/shift_left_2_PC.v
+++ modulos/shift_left_2_PC.v
@@ -1,6 +1,6 @@
 module shift_left_2_PC (
   input wire [25:0] data_in; //veio de instruction[25:0]
-  output wire [28:0] data_out; //vai para Mux 8x1 - PCSource (entrada 1)
+  output wire [28:0] data_out; //vai para jump_address_31_0 do mux_pcsource
 );
 
   assign data_out = {data_in, {2{1'b0}}; //concatena data_in (MSB) com o número 0 extendido para 2 bits (LSB)
