e6c845c6a3f6bf7d75de025c8dc248cd9b7137b6
Pedro Fonseca <86477558+pedroalfonseca@users.noreply.github.com>
2022-08-29 10:37:47 -0300

    Update shift_left_16.v

--- modulos/shift_left_16.v
+++ modulos/shift_left_16.v
@@ -1,6 +1,6 @@
 module shift_left_16 (
-  input wire [15:0] data_in; //vem de instruction[15:0]
-  output wire [31:0] data_out; //vai para Mux 8x1 - MemtoReg (entrada 6)
+  input wire [15:0] data_in; //vem de Instr15_0	do Instr_Reg.vhd
+  output wire [31:0] data_out; //vai para f do mux_memtoreg.v
 );
 
   assign data_out = {data_in, {16{1'b0}}; //concatena data_in (MSB) com o número 0 extendido em 16 bits (LSB)
