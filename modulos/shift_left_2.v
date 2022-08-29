4ba86b5485b0c3ca12e104a45e5aeec5782a23ab
Pedro Fonseca <86477558+pedroalfonseca@users.noreply.github.com>
2022-08-29 10:41:14 -0300

    Update shift_left_2.v

--- modulos/shift_left_2.v
+++ modulos/shift_left_2.v
@@ -1,6 +1,6 @@
 module shift_left_2 (
   input wire [31:0] data_in; //veio de sign_extend_16
-  output wire [31:0] data_out; //vai para Mux 4x1 - ALUSrcB (entrada 1)
+  output wire [31:0] data_out; //vai para c de mux_alusrb.v
 );
 
   assign data_out = data_in << 2; //desloca data_in 2 bits para a esquerda, colocando zeros à direita
