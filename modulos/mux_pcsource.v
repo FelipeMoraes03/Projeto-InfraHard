653fdbdbee72304ee25ca3162a5e4486469d9d14
Amanda-Quirino <93095388+Amanda-Quirino@users.noreply.github.com>
2022-08-29 10:51:31 -0300

    Update mux_pcsource.v

--- modulos/mux_pcsource.v
+++ modulos/mux_pcsource.v
@@ -7,16 +7,16 @@ module mux_pcsource(input wire [31:0] aluResult,
                      output reg [31:0] out
                      );
   
-  always@() begin
-    case (sel)
-      3'b000 out = aluResult;
-      3'b001 out = jump_address_31_0;
-      3'b010 out = memData_extended;
-      3'b011 out = aluOut;
-      3'b100 out = epc;
-      3'b101 out = 32'b0;
-      3'b110 out = 32'b0;
-      3'b111 out = 32'b0;
-    endcase
-  end
+	always@(*) begin
+		case (sel)
+			3'b000: out = aluResult;
+			3'b001: out = jump_address_31_0;
+			3'b010: out = memData_extended;
+			3'b011: out = aluOut;
+			3'b100: out = epc;
+			3'b101: out = 32'b0;
+			3'b110: out = 32'b0;
+			3'b111: out = 32'b0;
+		endcase
+	end
 endmodule
