9634d30e8ccbd75a2c6219ea6b5b5d0d35430ef4
Amanda-Quirino <93095388+Amanda-Quirino@users.noreply.github.com>
2022-08-29 11:06:39 -0300

    Update shift_left_2.v

--- modulos/shift_left_2.v
+++ modulos/shift_left_2.v
@@ -1,6 +1,6 @@
 module shift_left_2 (
-  input wire [31:0] data_in; //veio de sign_extend_16
-  output wire [31:0] data_out; //vai para c do mux_alusrb.v
+  input wire [31:0] data_in, //veio de sign_extend_16
+  output wire [31:0] data_out //vai para c do mux_alusrb.v
 );
 
   assign data_out = data_in << 2; //desloca data_in 2 bits para a esquerda, colocando zeros à direita
