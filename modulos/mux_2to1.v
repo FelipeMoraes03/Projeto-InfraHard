8bb635020ed9eb284bef9f1b944e93ff82853110
gabrielapinheiroc <mgpc1905@gmail.com>
2022-08-29 10:09:35 -0300

    hotfix: bugs fixed in mux and demux

--- modulos/mux_2to1.v
+++ modulos/mux_2to1.v
@@ -4,10 +4,10 @@ module mux_2to1(input wire [31:0] a,
                 output reg [31:0] out
                 );
   
-  always@() begin
+  always@(*) begin
     case (sel)
-      1'b0 out = a;
-      1'b1 out = b;
+      1'b0: out = a;
+      1'b1: out = b;
     endcase
   end
 endmodule
