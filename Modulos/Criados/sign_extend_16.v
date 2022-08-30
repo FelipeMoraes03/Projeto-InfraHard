0cd6f8d7696bc296bb2bd6800048f3ac25cc3057
Erlan Lira <94120629+erlanl@users.noreply.github.com>
2022-08-29 23:20:01 -0300

    arrumando erro de sintaxe

--- Modulos/Criados/sign_extend_16.v
+++ Modulos/Criados/sign_extend_16.v
@@ -3,6 +3,6 @@ module sign_extend_16 (
     output wire [31:0] Data_out
 );
 
-    assign Data_out = (Data_in[15]) ? {{16{1'b1}}, Data_in} : {{16{0'b1}}, Data_in};
+  assign Data_out = (Data_in[15]) ? {{16{1'b1}}, Data_in} : {16'b0, Data_in};
 
 endmodule
