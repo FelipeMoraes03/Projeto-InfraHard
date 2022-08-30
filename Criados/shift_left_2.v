1d222e3a67d9d6f8c42fc595598ed48a5e80c827
Erlan Lira <94120629+erlanl@users.noreply.github.com>
2022-08-29 22:31:09 -0300

    Delete Criados directory

--- Criados/shift_left_2.v
+++
@@ -1,9 +0,0 @@
-
-module shift_left_2 (
-  input wire [31:0] data_in, //veio de sign_extend_16
-  output wire [31:0] data_out //vai para c do mux_alusrb.v
-);
-
-  assign data_out = data_in << 2; //desloca data_in 2 bits para a esquerda, colocando zeros à direita
-
-endmodule
--- Criados/sign_extend_16.v
+++
@@ -1,8 +0,0 @@
-module sign_extend_16 (
-    input wire [15:0] Data_in,
-    output wire [31:0] Data_out
-);
-
-    assign Data_out = (Data_in[15]) ? {{16{1'b1}}, Data_in} : {{16{0'b1}}, Data_in};
-
-endmodule
