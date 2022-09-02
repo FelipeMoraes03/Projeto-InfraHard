b1309167a868b001145e8b496bbc4d6c8a72e0f8
Erlan Lira <94120629+erlanl@users.noreply.github.com>
2022-09-02 01:40:59 -0300

    Delete Modulos directory

--- Modulos/Criados/sign_extend_8.v
+++
@@ -1,8 +0,0 @@
-module zero_extender_8 (
-    input wire [7:0] Data_in,
-    output wire [31:0] Data_out
-);
-
-    assign Data_out = {24'b0, Data_in};
-
-endmodule
--- Modulos/Criados/zero_extender_1.v
+++
@@ -1,8 +0,0 @@
-module zero_extender_1 (
-    input wire Data_in,
-    output wire [31:0] Data_out
-);
-
-    assign Data_out = {31'b0, Data_in};
-
-endmodule
