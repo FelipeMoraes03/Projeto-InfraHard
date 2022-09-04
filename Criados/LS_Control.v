71821de9170c06180286c17bb7d0274877d1304f
Felipe Moraes <95718487+FelipeMoraes03@users.noreply.github.com>
2022-09-04 19:20:05 -0300

    LS_Control atualizado

--- Criados/LS_Control.v
+++ Criados/LS_Control.v
@@ -1,36 +1,42 @@
-module LS_Control (input [31:0] value,
+module LS_Control_ (input [31:0] b,
                   input [1:0] control,
                   input control2,
-                  input [31:0] instruction,
+                  input [31:0] mdr,
                   input clock,
                   input reset,
                   output reg [31:0] out);
-  
+
   always @(posedge clock)
     begin
       if(reset)
         out = 32'b0;
       
-      else
-        if (control2 == 1'b1)
-          if(control == 2'b01)
-            out =  {value[7:0], instruction[23:0]}; // Correto
-                          
-          else if (control == 2'b10)
-            out = {value[14:0] , instruction[16:0]}; // Correto
-
-          else if(control == 2'b11)
-            out = value; // Correto
-    
-        else
-          if(control == 2'b01)
-            out =  {{23{1'b0}}, value[7:0]};
+      else begin
+        if (control2 == 1'b1) begin
+          if(control == 2'b01) begin
+            out =  {b[7:0], mdr[23:0]}; // Correto
+          end    
+          else if (control == 2'b10) begin
+            out = {b[15:0] , mdr[15:0]}; // Correto
+          end
+          else if(control == 2'b11) begin
+            out = b; // Correto
+          end
+        end    
+        else begin
+          if(control == 2'b01) begin
+            out =  {24'b0, mdr[7:0]};
+          end
                                 
-          else if (control == 2'b10)
-            out = {{16{1'b0}}, value[14:0]};
+          else if (control == 2'b10) begin
+            out = {16'b0, mdr[15:0]};
+          end
 
-          else if(control == 2'b11)
-            out = value;
+          else if(control == 2'b11) begin
+            out = mdr;
+          end
+        end
+      end
     end
   
 endmodule
