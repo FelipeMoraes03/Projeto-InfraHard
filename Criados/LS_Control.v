06bc4788e9f39c4fd058bdc4f68ef07efe94126c
Guilherme Maciel <85590696+GuilhermeMaciel75@users.noreply.github.com>
2022-09-04 09:48:36 -0300

    LS_Control_atualizado

--- Criados/LS_Control.v
+++ Criados/LS_Control.v
@@ -1,26 +1,24 @@
-module LScontrol (input [31:0] value,
+module LS_Control (input [31:0] value,
                   input [1:0] control,
                   input [31:0] instruction,
                   input clock,
                   input reset,
                   output reg [31:0] out);
   
-  always @(posedge clock or negedge reset)
+  always @(posedge clock)
     begin
-      if(~reset)
-        out <= 32'b0;
+      if(reset)
+        out = 32'b0;
       
       else
         if(control == 2'b01)
-          
-          out <=  {value[31:23], instruction[22:0]};
+          out =  {value[7:0], instruction[23:0]};
                                 
         else if (control == 2'b10)
-          out <= {value[31:15] , instruction[14:0]};
-      	
-          
+      	  out = {value[14:0] , instruction[17:0]};
+
         else if(control == 2'b11)
-          out <= value;
+          out = value;
       
     end
 endmodule
