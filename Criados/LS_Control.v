30bbab1c7244159d726fafce423b9431ce84538b
Erlan Lira <94120629+erlanl@users.noreply.github.com>
2022-09-04 16:07:54 -0300

    att

--- Criados/LS_Control.v
+++ Criados/LS_Control.v
@@ -1,5 +1,6 @@
 module LS_Control (input [31:0] value,
                   input [1:0] control,
+                  input control2,
                   input [31:0] instruction,
                   input clock,
                   input reset,
@@ -11,14 +12,25 @@ module LS_Control (input [31:0] value,
         out = 32'b0;
       
       else
-        if(control == 2'b01)
-          out =  {value[7:0], instruction[23:0]};
-                                
-        else if (control == 2'b10)
-      	  out = {value[14:0] , instruction[17:0]};
+        if (control2 == 1'b1)
+          if(control == 2'b01)
+            out =  {value[7:0], instruction[23:0]}; // Correto
+                          
+          else if (control == 2'b10)
+            out = {value[14:0] , instruction[16:0]}; // Correto
 
-        else if(control == 2'b11)
-          out = value;
-      
+          else if(control == 2'b11)
+            out = value; // Correto
+    
+        else
+          if(control == 2'b01)
+            out =  {{23{1'b0}}, value[7:0]};
+                                
+          else if (control == 2'b10)
+            out = {{16{1'b0}}, value[14:0]};
+
+          else if(control == 2'b11)
+            out = value;
     end
+  
 endmodule
